6aca16f5f0a5b2cc6ea2534d78c0830e8f562f5e
Arman-Nandeshwar <147031134+Arman-Nandeshwar@users.noreply.github.com>
2024-06-30 15:40:03 +0000

    comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" >1);
+// console.log("02" >1);
+
+//THIS TYPE OF COMPARISON SHOULD BE AVOIDED
+console.log(null>0);
+console.log(null == 0); //equality check
+console.log(null >= 0);
+//equality check and comparison operator works differently
+
+console.log(undefined == 0);
+console.log(undefined >0);
+console.log(undefined < 0);
+
+//===
+console.log("2" === 2);
