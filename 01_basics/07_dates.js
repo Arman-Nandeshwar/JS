c70ac5b4cbabb0a206672a4b904e50f10b40a1f9
Arman-Nandeshwar <147031134+Arman-Nandeshwar@users.noreply.github.com>
2024-07-01 02:39:23 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,25 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate =  new Date(2023,0,23) //month start from 0
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate =  new Date(2023,0,23,5,3)
+let myCreatedDate =  new Date("01-14-2023")
+// let myCreatedDate =  new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now() //gives current date
+// console.log(myTimeStamp); //in millisecond
+// console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000)); //in seconds
+
+let newDate =  new Date()
+console.log(newDate)
