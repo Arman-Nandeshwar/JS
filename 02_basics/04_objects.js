92aba6c50b8c4771f4f733453934e8e83ca9835c
Arman-Nandeshwar <147031134+Arman-Nandeshwar@users.noreply.github.com>
2024-07-01 16:59:35 +0000

    objects 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+//singleton : constructor
+
+// const tinderUser = new Object() //singleton object
+const tinderUser ={} //non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "arman"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "arman",
+            lastname : "nandeshwar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+// const obj3 = {obj1,obj2}
+
+// const obj3 = Object.assign({},obj1,obj2) //merge  objects
+
+const obj3 = {...obj1, ...obj2} //spread operator :latest
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email : "h@gamil.com"
+    },
+    {
+        id: 1,
+        email : "h@gamil.com"
+    },
+    {
+        id: 1,
+        email : "h@gamil.com"
+    }
+]
+// console.log(users[1].email);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser)); //returns array of keys
+console.log(Object.values(tinderUser)); //returns array of values
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); //to check if the object has that property
