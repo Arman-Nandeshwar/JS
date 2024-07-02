c01bda50845f06bd05b87a4c6df0c3553b8bad94
Arman-Nandeshwar <147031134+Arman-Nandeshwar@users.noreply.github.com>
2024-07-02 15:21:16 +0000

    control flow

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,45 @@
+// const userEmail = "h@gmail.ai"
+const userEmail = []
+
+if(userEmail){
+    console.log("got user email");
+}
+else{
+    console.log("dont have user email");
+}
+
+//falsy values
+// false, 0 ,-0 , BigInt 0n, "", null , undefined, NaN
+
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){} 
+//[anything inside the sting is truthy values except empty string]
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+
+//Nullish Coalescing Operator (??) : only for : null , undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20  //in this the first value gets assigned
+
+// console.log(val1);
+
+
+
+//TERNARY OPERATOR
+//condition ? true :false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
