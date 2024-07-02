5340f0fbb77715db15b6741adbebb06a52b2cae7
Arman-Nandeshwar <147031134+Arman-Nandeshwar@users.noreply.github.com>
2024-07-02 02:46:32 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,52 @@ if(true){
     let a =10
     const b =20
     // var c = 30 //
-    console.log("INNER : ",a);
+    // console.log("INNER : ",a);
  }
 
- console.log(a);
+//  console.log(a);
 //  console.log(b);
 //  console.log(c);
+
+
+//inner function can access variables of outer function 
+//but viceversa is not true
+function one(){
+    const username = "arman"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    //// console.log(website);
+    two()
+}
+one()
+
+
+ if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website  = "youtube"
+        // console.log(username + website);
+    }
+    //// console.log(website);
+ }
+
+////  console.log(username);
+
+//+++++++++++++++++++ interesting ++++++++++++++++
+//two types of function declaration
+
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+
+
+
+
+addtwo(5)
+const addtwo = function(num){
+    return num +2
+}
